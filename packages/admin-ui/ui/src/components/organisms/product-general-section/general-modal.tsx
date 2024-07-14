fd1eeccbf4304d34a58b81bc6737941f191193c7
Johannes Spijkerman <smitjohannes96@icloud.com>
2024-07-14 19:20:35 +0200

    Fix edit window when editing product without existing variant

--- packages/admin-ui/ui/src/components/organisms/product-general-section/general-modal.tsx
+++ packages/admin-ui/ui/src/components/organisms/product-general-section/general-modal.tsx
@@ -1,184 +1,204 @@
-import { useTranslation } from "react-i18next"
-import GeneralForm, { GeneralFormType } from "../../forms/product/general-form"
+import {useTranslation} from "react-i18next"
+import GeneralForm, {GeneralFormType} from "../../forms/product/general-form"
 import OrganizeForm, {
-  OrganizeFormType,
+    OrganizeFormType,
 } from "../../forms/product/organize-form"
 
-import { Product } from "@medusajs/medusa"
-import { useEffect } from "react"
-import { useForm } from "react-hook-form"
+import {Product} from "@medusajs/medusa"
+import {useEffect} from "react"
+import {useForm} from "react-hook-form"
 import useEditProductActions from "../../../hooks/use-edit-product-actions"
-import { nestedForm } from "../../../utils/nested-form"
+import {nestedForm} from "../../../utils/nested-form"
 import Button from "../../fundamentals/button"
 import Modal from "../../molecules/modal"
 import StockPriceForm, {StockPriceFormType} from "../../forms/de-geslepen-steen/product/stock-price-form";
 import DimensionsForm, {DimensionsFormType} from "../../forms/product/dimensions-form";
 
 type Props = {
-  product: Product
-  open: boolean
-  onClose: () => void
+    product: Product
+    open: boolean
+    onClose: () => void
 }
 
 type GeneralFormWrapper = {
-  dimensions: DimensionsFormType
-  general: GeneralFormType
-  organize: OrganizeFormType
-  stockPrice: StockPriceFormType
+    dimensions: DimensionsFormType
+    general: GeneralFormType
+    organize: OrganizeFormType
+    stockPrice: StockPriceFormType
 }
 
-const GeneralModal = ({ product, open, onClose }: Props) => {
-  const { t } = useTranslation()
-  const { onUpdate, updating } = useEditProductActions(product.id)
-  const form = useForm<GeneralFormWrapper>({
-    defaultValues: getDefaultValues(product),
-  })
+const GeneralModal = ({product, open, onClose}: Props) => {
+    const {t} = useTranslation()
+    const {onUpdate, updating} = useEditProductActions(product.id)
+    const form = useForm<GeneralFormWrapper>({
+        defaultValues: getDefaultValues(product),
+    })
 
-  const {
-    formState: { isDirty },
-    handleSubmit,
-    reset,
-  } = form
+    const {
+        formState: {isDirty},
+        handleSubmit,
+        reset,
+    } = form
 
-  useEffect(() => {
-    reset(getDefaultValues(product))
-  }, [product, reset])
+    useEffect(() => {
+        reset(getDefaultValues(product))
+    }, [product, reset])
 
-  const onReset = () => {
-    reset(getDefaultValues(product))
-    onClose()
-  }
+    const onReset = () => {
+        reset(getDefaultValues(product))
+        onClose()
+    }
 
-  const onSubmit = handleSubmit((data) => {
-    onUpdate(
-      {
-        title: data.general.title,
-        handle: data.general.handle,
-        // @ts-ignore
-        material: data.general.material,
-        // @ts-ignore
-        subtitle: data.general.subtitle,
-        // @ts-ignore
-        description: data.general.description,
-        // @ts-ignore
-        tags: data.organize.tags
-          ? data.organize.tags.map((t) => ({ value: t }))
-          : null,
-        categories: data.organize?.categories?.length
-          ? data.organize.categories.map((id) => ({ id }))
-          : [],
-        height: data.dimensions.height || undefined,
-        length: data.dimensions.length || undefined,
-        weight: data.dimensions.weight || undefined,
-        width: data.dimensions.width || undefined,
-        diameter: data.dimensions.diameter || undefined,
-        variants: [
-          {
-            id: product.variants[0].id,
-            inventory_quantity: data.stockPrice.stock,
-            prices: [
-              {
-                id: product.variants[0].prices[0].id,
-                amount: data.stockPrice.price,
-                currency_code: product.variants[0].prices[0].currency_code
-              }
-            ],
-          },
-        ],
-      },
-      onReset
+    const onSubmit = handleSubmit((data) => {
+        onUpdate(
+            {
+                title: data.general.title,
+                handle: data.general.handle,
+                // @ts-ignore
+                material: data.general.material,
+                // @ts-ignore
+                subtitle: data.general.subtitle,
+                // @ts-ignore
+                description: data.general.description,
+                // @ts-ignore
+                tags: data.organize.tags
+                    ? data.organize.tags.map((t) => ({value: t}))
+                    : null,
+                categories: data.organize?.categories?.length
+                    ? data.organize.categories.map((id) => ({id}))
+                    : [],
+                height: data.dimensions.height || undefined,
+                length: data.dimensions.length || undefined,
+                weight: data.dimensions.weight || undefined,
+                width: data.dimensions.width || undefined,
+                diameter: data.dimensions.diameter || undefined,
+                variants: product.variants.length === 0
+                    ? [
+                        {
+                            title: 'internal',
+                            inventory_quantity: data.stockPrice.stock ?? 0,
+                            prices: [
+                                {
+                                    amount: data.stockPrice.price,
+                                    currency_code: 'eur'
+                                }
+                            ],
+                            allow_backorder: false,
+                            options: [
+                                {
+                                    value: 'internal'
+                                }
+                            ],
+                            manage_inventory: true
+                        },
+                    ] :
+                    [
+                        {
+                            id: product.variants[0].id ?? null,
+                            inventory_quantity: data.stockPrice.stock,
+                            prices: [
+                                {
+                                    id: product.variants[0].prices[0].id ?? null,
+                                    amount: data.stockPrice.price,
+                                    currency_code: product.variants[0].prices[0].currency_code ?? 'eur',
+                                }
+                            ],
+                        }
+                    ],
+            },
+            onReset
+        )
+    })
+
+    return (
+        <Modal open={open} handleClose={onReset} isLargeModal>
+            <Modal.Body>
+                <Modal.Header handleClose={onReset}>
+                    <h1 className="inter-xlarge-semibold m-0">
+                        {t(
+                            "product-general-section-edit-general-information",
+                            "Edit General Information"
+                        )}
+                    </h1>
+                </Modal.Header>
+                <form onSubmit={onSubmit}>
+                    <Modal.Content>
+                        <GeneralForm
+                            form={nestedForm(form, "general")}
+                            isGiftCard={product.is_giftcard}
+                        />
+                        <div className="my-xlarge">
+                            <StockPriceForm form={nestedForm(form, "stockPrice")}/>
+                        </div>
+                        <div className="my-xlarge">
+                            <h3 className="inter-base-semibold mb-base">
+                                {t("new-dimensions", "Dimensions")}
+                            </h3>
+                            <DimensionsForm form={nestedForm(form, "dimensions")}/>
+                        </div>
+                        <div className="my-xlarge">
+                            <h2 className="inter-base-semibold mb-base">
+                                Organize{" "}
+                                {product.is_giftcard
+                                    ? t("product-general-section-gift-card", "Gift Card")
+                                    : t("product-general-section-product", "Product")}
+                            </h2>
+                            <OrganizeForm form={nestedForm(form, "organize")}/>
+                        </div>
+                    </Modal.Content>
+                    <Modal.Footer>
+                        <div className="flex w-full justify-end gap-x-2">
+                            <Button
+                                size="small"
+                                variant="secondary"
+                                type="button"
+                                onClick={onReset}
+                            >
+                                {t("product-general-section-cancel", "Cancel")}
+                            </Button>
+                            <Button
+                                size="small"
+                                variant="primary"
+                                type="submit"
+                                disabled={!isDirty}
+                                loading={updating}
+                            >
+                                {t("product-general-section-save", "Save")}
+                            </Button>
+                        </div>
+                    </Modal.Footer>
+                </form>
+            </Modal.Body>
+        </Modal>
     )
-  })
-
-  return (
-    <Modal open={open} handleClose={onReset} isLargeModal>
-      <Modal.Body>
-        <Modal.Header handleClose={onReset}>
-          <h1 className="inter-xlarge-semibold m-0">
-            {t(
-              "product-general-section-edit-general-information",
-              "Edit General Information"
-            )}
-          </h1>
-        </Modal.Header>
-        <form onSubmit={onSubmit}>
-          <Modal.Content>
-            <GeneralForm
-                form={nestedForm(form, "general")}
-                isGiftCard={product.is_giftcard}
-            />
-            <div className="my-xlarge">
-              <StockPriceForm form={nestedForm(form, "stockPrice")}/>
-            </div>
-            <div className="my-xlarge">
-              <h3 className="inter-base-semibold mb-base">
-                {t("new-dimensions", "Dimensions")}
-              </h3>
-              <DimensionsForm form={nestedForm(form, "dimensions")}/>
-            </div>
-            <div className="my-xlarge">
-              <h2 className="inter-base-semibold mb-base">
-                Organize{" "}
-                {product.is_giftcard
-                    ? t("product-general-section-gift-card", "Gift Card")
-                    : t("product-general-section-product", "Product")}
-              </h2>
-              <OrganizeForm form={nestedForm(form, "organize")}/>
-            </div>
-          </Modal.Content>
-          <Modal.Footer>
-            <div className="flex w-full justify-end gap-x-2">
-              <Button
-                  size="small"
-                  variant="secondary"
-                  type="button"
-                  onClick={onReset}
-              >
-                {t("product-general-section-cancel", "Cancel")}
-              </Button>
-              <Button
-                  size="small"
-                  variant="primary"
-                  type="submit"
-                  disabled={!isDirty}
-                  loading={updating}
-              >
-              {t("product-general-section-save", "Save")}
-              </Button>
-            </div>
-          </Modal.Footer>
-        </form>
-      </Modal.Body>
-    </Modal>
-  )
 }
 
 const getDefaultValues = (product: Product): GeneralFormWrapper => {
-  return {
-    general: {
-      title: product.title,
-      subtitle: product.subtitle,
-      material: product.material,
-      handle: product.handle!,
-      description: product.description || null,
-    },
-    organize: {
-      tags: product.tags ? product.tags.map((t) => t.value) : null,
-      categories: product.categories?.map((c) => c.id),
-    },
-    stockPrice: {
-      stock: product.variants[0].inventory_quantity,
-      price: product.variants[0].prices[0].amount / 100,
-      origin_country: product.origin_country
-    },
-    dimensions: {
-      height: product.height ? product.height / 10 : null,
-      width: product.width ? product.width / 10 : null,
-      length: product.length ? product.length / 10 : null,
-      weight: product.weight,
-      diameter: product.diameter ? product.diameter / 10 : null
+    return {
+        general: {
+            title: product.title,
+            subtitle: product.subtitle,
+            material: product.material,
+            handle: product.handle!,
+            description: product.description || null,
+        },
+        organize: {
+            tags: product.tags ? product.tags.map((t) => t.value) : null,
+            categories: product.categories?.map((c) => c.id),
+        },
+        stockPrice: {
+            stock: product.variants.length ? product.variants[0].inventory_quantity : 1,
+            price: product.variants.length ? product.variants[0].prices[0].amount / 100 : 0,
+            origin_country: product.origin_country
+        },
+        dimensions: {
+            height: product.height ? product.height / 10 : null,
+            width: product.width ? product.width / 10 : null,
+            length: product.length ? product.length / 10 : null,
+            weight: product.weight,
+            diameter: product.diameter ? product.diameter / 10 : null
+        }
     }
-  }
 }
 
 export default GeneralModal
--- packages/admin-ui/ui/src/domain/products/edit/index.tsx
+++ packages/admin-ui/ui/src/domain/products/edit/index.tsx
@@ -1,5 +1,5 @@
-import { useAdminProduct } from "medusa-react"
-import { useNavigate, useParams } from "react-router-dom"
+import {useAdminCreateProductOption, useAdminCreateVariant, useAdminProduct} from "medusa-react"
+import {useNavigate, useParams} from "react-router-dom"
 import BackButton from "../../../components/atoms/back-button"
 import Spinner from "../../../components/atoms/spinner"
 import WidgetContainer from "../../../components/extensions/widget-container"
@@ -7,81 +7,125 @@ import ProductGeneralSection from "../../../components/organisms/product-general
 import ProductMediaSection from "../../../components/organisms/product-media-section"
 import ProductRawSection from "../../../components/organisms/product-raw-section"
 import ProductThumbnailSection from "../../../components/organisms/product-thumbnail-section"
-import { useWidgets } from "../../../providers/widget-provider"
-import { getErrorStatus } from "../../../utils/get-error-status"
+import {useWidgets} from "../../../providers/widget-provider"
+import {getErrorStatus} from "../../../utils/get-error-status"
+import {useEffect} from "react";
 
 const Edit = () => {
-  const { id } = useParams()
-  const navigate = useNavigate()
+    const {id} = useParams()
+    const navigate = useNavigate()
 
-  const { getWidgets } = useWidgets()
+    const {getWidgets} = useWidgets()
 
-  const { product, status, error } = useAdminProduct(id || "")
+    const {product, status, error, refetch} = useAdminProduct(id || "")
+    const createOption = useAdminCreateProductOption(id || "");
+    const createVariant = useAdminCreateVariant(id || "");
 
-  if (error) {
-    const errorStatus = getErrorStatus(error)
+    useEffect(() => {
+        if (product?.variants.length === 1) {
+            return;
+        }
 
-    if (errorStatus) {
-      // If the product is not found, redirect to the 404 page
-      if (errorStatus.status === 404) {
-        navigate("/404")
-        return null
-      }
+        if (product?.options.length === 0) {
+            createOption.mutate(
+                {
+                    title: 'internal'
+                },
+                {
+                    onSuccess: async () => {
+                        await refetch()
+                    }
+                })
+        }
+
+        if (product?.variants.length === 0) {
+            createVariant.mutate({
+                title: 'internal',
+                inventory_quantity: 0,
+                prices: [
+                    {
+                        amount: 0,
+                        currency_code: 'eur'
+                    }
+                ],
+                allow_backorder: false,
+                options: [
+                    {
+                        option_id: product.options[0].id,
+                        value: 'internal'
+                    }
+                ],
+                manage_inventory: true
+            }, {
+                onSuccess: async () => await refetch()
+            })
+        }
+    }, [product]);
+
+    if (error) {
+        const errorStatus = getErrorStatus(error)
+
+        if (errorStatus) {
+            // If the product is not found, redirect to the 404 page
+            if (errorStatus.status === 404) {
+                navigate("/404")
+                return null
+            }
+        }
+
+        // Let the error boundary handle the error
+        throw error
     }
 
-    // Let the error boundary handle the error
-    throw error
-  }
+    if (status === "loading" || !product) {
+        return (
+            <div className="flex h-[calc(100vh-64px)] w-full items-center justify-center">
+                <Spinner variant="secondary"/>
+            </div>
+        )
+    }
 
-  if (status === "loading" || !product) {
     return (
-      <div className="flex h-[calc(100vh-64px)] w-full items-center justify-center">
-        <Spinner variant="secondary" />
-      </div>
-    )
-  }
-
-  return (
-    <div className="pb-5xlarge">
-      <BackButton
-        path="/a/products"
-        label="Back to Products"
-        className="mb-xsmall"
-      />
-      <div className="gap-y-xsmall flex flex-col">
-        {getWidgets("product.details.before").map((w, i) => {
-          return (
-            <WidgetContainer
-              key={i}
-              injectionZone={"product.details.before"}
-              widget={w}
-              entity={product}
+        <div className="pb-5xlarge">
+            <BackButton
+                path="/a/products"
+                label="Back to Products"
+                className="mb-xsmall"
             />
-          )
-        })}
-        <div className="gap-x-base grid grid-cols-12">
-          <div className="gap-y-xsmall col-span-8 flex flex-col">
-            <ProductGeneralSection product={product} />
-            {getWidgets("product.details.after").map((w, i) => {
-              return (
-                <WidgetContainer
-                  key={i}
-                  injectionZone={"product.details.after"}
-                  widget={w}
-                  entity={product}
-                />
-              )
-            })}
-            <ProductRawSection product={product} />
-          </div>
-          <div className="gap-y-xsmall col-span-4 flex flex-col">
-            <ProductThumbnailSection product={product} />
-            <ProductMediaSection product={product} />
-          </div>
+            <div className="gap-y-xsmall flex flex-col">
+                {getWidgets("product.details.before").map((w, i) => {
+                    return (
+                        <WidgetContainer
+                            key={i}
+                            injectionZone={"product.details.before"}
+                            widget={w}
+                            entity={product}
+                        />
+                    )
+                })}
+                <div className="gap-x-base grid grid-cols-12">
+                    <div className="gap-y-xsmall col-span-8 flex flex-col">
+                        <ProductGeneralSection product={product}/>
+                        {getWidgets("product.details.after").map((w, i) => {
+                            return (
+                                <WidgetContainer
+                                    key={i}
+                                    injectionZone={"product.details.after"}
+                                    widget={w}
+                                    entity={product}
+                                />
+                            )
+                        })}
+                        <ProductRawSection product={product}/>
+                    </div>
+                    <div className="gap-y-xsmall col-span-4 flex flex-col">
+                        <ProductThumbnailSection product={product}/>
+                        <ProductMediaSection product={product}/>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Edit
